9c447f61ba25886890338694366580edf7960e42
Jasminems <jasmine.srakj@gmail.com>
2021-07-08 02:09:43 +0530

    Public

---
+++ Publicaccess/src/p1/A.java
@@ -0,0 +1,12 @@
+/*A Java program to create public access modifiers*/
+//creating package1
+package p1;
+public class A 
+{
+	public int a,b;
+	public int add(int a,int b)
+	{
+		return a+b;
+	}
+	
+}
---
+++ Publicaccess/src/p2/Test.java
@@ -0,0 +1,13 @@
+/*A Java program to create public access modifiers*/
+//creating package2
+package p2;
+//Importing package1
+import p1.*;
+public class Test 
+{
+	public static void main(String[] args) 
+	{
+		A ob=new A();
+		System.out.println(ob.add(10,20));
+		}
+}
